7f4420f84de48871a34bcda57ff44a55daf5e156
AngelosGian <angelos.giannourakos@gmail.com>
2023-01-18 18:05:35 +0200

    adding files first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_-+={}[]?>.,<;'
+const pressButton = document.getElementById('press-button')
+const output = document.getElementById('result')
+
+pressButton.addEventListener('click', randomize)
+
+function randomize(){
+    let randomChar = '';    
+    for(let i=0; i <= 12; i++){
+        randomChar += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return output.innerHTML = randomChar;
+}
